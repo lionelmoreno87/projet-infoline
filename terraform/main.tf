cb2321e8738e6bf3fab4981b1787fbffd91e07cf
lionelmoreno87 <mor3nolionel@gmail.com>
2026-01-17 13:50:02 +0100

    feat: terraform version + provider AWS

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+# Version de Terraform
+
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Choix du provider
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+    }
+  }
+}
