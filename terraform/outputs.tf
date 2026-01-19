1bf8fb2b71fdc0c039f3265536c280e4bc84d061
lionelmoreno87 <mor3nolionel@gmail.com>
2026-01-19 08:41:06 +0100

    feat: Informations outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,36 @@
+# Informations VPC
+
+
+output "vpc_id" {
+  description = "ID du VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  description = "ID du subnet public"
+  value       = aws_subnet.public.id
+}
+
+# Informations EC2
+
+output "ec2_spot_request_id" {
+  description = "ID de la requête Spot"
+  value       = aws_spot_instance_request.k3s.id
+}
+
+output "ec2_instance_id" {
+  description = "ID de l'instance EC2"
+  value       = aws_spot_instance_request.k3s.spot_instance_id
+}
+
+output "ec2_public_ip" {
+  description = "IP publique (Elastic IP) de l'instance K3s"
+  value       = aws_eip.k3s.public_ip
+}
+
+# Commandes de connexion
+
+output "ssm_connection_command" {
+  description = "Commande pour se connecter via SSM"
+  value       = "aws ssm start-session --target ${aws_spot_instance_request.k3s.spot_instance_id} --region ${var.aws_region}"
+}
