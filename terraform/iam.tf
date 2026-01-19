51c316e20b2f47e8253133fb618371f52386214e
lionelmoreno87 <mor3nolionel@gmail.com>
2026-01-19 06:34:47 +0100

    feat(terraform): IAM Role pour EC2 (SSM)

---
+++ terraform/iam.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_role" "k3s" {
+  name = "${var.project_name}-k3s-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+
+  tags = {
+    Name = "${var.project_name}-k3s-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.k3s.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+resource "aws_iam_instance_profile" "k3s" {
+  name = "${var.project_name}-k3s-profile"
+  role = aws_iam_role.k3s.name
+}
